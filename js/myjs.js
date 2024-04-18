663ec753c031e20bcdfe53dcfa92f40f788c44ea
Arshia keshavarz motamedi <156549882+arshiaMotamedi@users.noreply.github.com>
2024-04-17 22:41:12 -0500

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hourlyRate = document.getElementById("hourlyRate");
+const hourlyRate = document.getElementById("hourlyRate");
 
-  document.querySelectorAll('input[name="reason"]').forEach((radio) => {
-    radio.addEventListener("change", () => {
-      if (radio.value === "Hiring") {
-        hourlyRate.style.display = "block";
-      } else {
-        hourlyRate.style.display = "none";
-      }
-    });
+document.querySelectorAll('input[name="reason"]').forEach((radio) => {
+  radio.addEventListener("change", () => {
+    if (radio.value === "Hiring") {
+      hourlyRate.style.display = "block";
+    } else {
+      hourlyRate.style.display = "none";
+    }
   });
 });
